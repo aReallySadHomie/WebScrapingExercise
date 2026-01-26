1df528ee04eb5ddf83ef96dc9ad708092320fc49
Samu <Samuele@DESKTOP-SAM.(none)>
2026-01-27 00:19:15 +0100

    Implementazione della barra dei progressi, fix della regex di prezzo

--- BooksToScrape.py
+++ BooksToScrape.py
@@ -2,36 +2,39 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 from DeepBookScraper import *
+from tqdm import tqdm
 
 # TODO scraping delle pagine max, oppure provare while response.status_code == 200 o altri HTTP response status codes
-MAXPAGES = 50
+MAXPAGES = 3
 
 # sistemare i parametri, impostare i default
 def book_scraper():
 
     base_url = "http://books.toscrape.com/catalogue/"
-    start_url = "http://books.toscrape.com/catalogue/page-1.html"
     all_books = []
 
-    current_page = 1
-    while current_page <= MAXPAGES:
-        print(f"scanning page {current_page}/{MAXPAGES}")
+    for current_page in tqdm(range(1, MAXPAGES+1), desc = "Total progression", position=0):
         url = f"{base_url}page-{current_page}.html"
-        response = requests.get(url)
-        #TODO try except usando il lxml anziché l'html.parser
-        soup = BeautifulSoup(response.text, "html.parser")
+        response = requests.get(url, timeout=5)
+        #TODO try except usando il lxml + l'html.parser
+        soup = BeautifulSoup(response.text, "lxml")
         listed_books = soup.find_all("article", class_="product_pod")
 
-        for book in listed_books:
+        for book in tqdm(listed_books, desc = f"Now scraping elements from page {current_page}",position = 1, leave = False):
             book_url_snippet = book.h3.a.get("href", "URL not found")
             book_url = base_url + book_url_snippet if book_url_snippet != "URL not found" else ""
 
-            book_details = deep_book_scraper(book_url)
-            all_books.append(book_details)
+            book_objects = deep_book_scraper(book_url)
+            all_books.append(book_objects)
 
+    print(f"Found {len(all_books)} books:\n")
 
-
-        current_page += 1
+    return all_books
 
 if __name__== '__main__':
-    book_scraper()
+    detailed_book_list = book_scraper()
+    for element in detailed_book_list:
+        print(element)
+
+    print("\n\n")
+    input("Press enter to exit...")
--- DeepBookScraper.py
+++ DeepBookScraper.py
@@ -1,3 +1,6 @@
+from turtledemo.paint import switchupdown
+from unittest import case
+
 from bs4 import BeautifulSoup
 import requests
 import re
@@ -6,38 +9,59 @@ class Book:
     def __init__(self, title, price, rating, availability, upc, description):
         self.title = title
         self.price = self.clean_price(price)
-        self.rating = rating
+        self.rating = self.numeric_rating(rating)
         self.availability = self.clean_availability(availability)
         self.upc = upc
         self.description = description
 
     @staticmethod
     def clean_price(price):
-        return float(price.replace(",", ".").replace("£", ""))
+        #TODO sistemare la regex per includere i punti e le virgole
+        return float(re.search(r'(\d,)*\d+[,.]\d*', price).group())
 
     @staticmethod
     def clean_availability(availability):
         return int(re.search(r'\d+', availability).group())
 
+    @staticmethod
+    def numeric_rating(rating):
+        match rating:
+            case "One":
+                return 1
+            case "Two":
+                return 2
+            case "Three":
+                return 3
+            case "Four":
+                return 4
+            case "Five":
+                return 5
+        return 0
+
     def __repr__(self):
-        return f"<Book: {self.title}, {self.price}£, {self.rating}, {self.availability}, {self.upc}>"
+        return (f"Book: {self.title},\n"
+                f"Price: {self.price}£,\n"
+                f"Description: {self.description[:50]}...\n"
+                f"Rating: {self.rating},\n"
+                f"Availability: {self.availability},\n"
+                f"UPC: {self.upc}\n")
 
 
 
-def deep_book_scraper(url):
+def deep_book_scraper(url) -> Book:
     #TODO gestire eventuali URL mancanti
-    response = requests.get(url)
+    response = requests.get(url, timeout=5)
     soup = BeautifulSoup(response.text, "lxml")
     article = soup.find("article", class_="product_page")
 
     title = article.find("div", class_="col-sm-6 product_main").h1.get_text()
     price = article.find("p", class_="price_color").get_text()
     rating = article.find("p", class_="star-rating")["class"][1]
-    description = article.find("p").get_text()
+    description = article.find_all("p")[3].get_text() # hardcoded, non ne vado fiero
 
     table = article.find("table", class_="table")
     rows = table.find_all("tr")
-    upc = table.find_all("td").text
+    upc = rows[0].td.text
     availability = rows[5].td.get_text()
 
     return Book(title = title,
