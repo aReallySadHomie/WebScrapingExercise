c8c13aac4ff6314f4f303cd36a743b9b56e837d4
Samu <Samuele@DESKTOP-SAM.(none)>
2026-01-28 13:11:03 +0100

    multithreading finito

--- BooksToScrape.py
+++ BooksToScrape.py
@@ -1,83 +1,57 @@
 import os.path
 import csv
-from DeepBookScraper import *
+import threading
 from tqdm import tqdm
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import ThreadPoolExecutor
 
-# TODO scraping delle pagine max, oppure provare while response.status_code == 200 o altri HTTP response status codes
+from new_directory.PageProcessor import process_page
+
+# TODO scraping delle pagine e libri max, oppure provare response.raise_for_status()
 # TODO passare da csv a PostgreSQL
 MAXPAGES = 50
+MAXBOOKS = 1000
 BOOKS_FILENAME = "books.csv"
 MAX_THREADS = 10
+csv.lock = threading.Lock()
+
 # sistemare i parametri, impostare i default
 def book_scraper():
 
     base_url = "http://books.toscrape.com/catalogue/"
     all_books = []
-
+    seen_books = set()
     csv_header = ["Title", "Price(£)","Description", "Rating", "Availability", "UPC", "URL"]
+    progress_bar_pages = tqdm(range(1, MAXPAGES+1), desc = "Scanned pages", position=0, leave=True)
+    progress_bar_books = tqdm(range(1, MAXBOOKS+1), desc = "Scanned books",position=1 , leave=True)
+
     if not os.path.exists(BOOKS_FILENAME):
         with open(BOOKS_FILENAME, "w", newline="", encoding="UTF-8-sig") as csvfile:
             writer = csv.writer(csvfile, delimiter= ";")
             writer.writerow(csv_header)
 
-    progress_bar_pages = tqdm(range(1, MAXPAGES+1), desc = "Total progression", position=0)
     # TODO multithread in diverse pagine
-    for current_page in progress_bar_pages:
 
-        try:
-            url = f"{base_url}page-{current_page}.html"
-            response = requests.get(url, timeout=5)
-        except Exception as e:
-            print(f"Something is wrong with the HTTP request, go fix it... See you later\n{e}")
-            break
+    with open(BOOKS_FILENAME, "a", newline="", encoding="UTF-8-sig") as csvfile:
+        writer = csv.writer(csvfile, delimiter=";", quotechar='"')
 
-        try:
-            soup = BeautifulSoup(response.text, "lxml")
-        except Exception as e:
-            tqdm.write(f"Warning: lxml failed, using html.parser. Error: {e}")
-            soup = BeautifulSoup(response.text, "html.parser")
+        with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
+            executor.map(lambda page: process_page(page, base_url, writer, progress_bar_pages, progress_bar_books, all_books, seen_books),
+                         range(1, MAXPAGES+1))
 
-        listed_books = soup.find_all("article", class_="product_pod")
-
-        # Implementiamo sto multithreading
-        books_urls = []
-        for book in listed_books:
-            book_url_snippet = book.h3.a.get("href", "URL not found")
-            if book_url_snippet != "URL not found":
-                book_url = base_url + book_url_snippet
-            else:
-                continue
-            books_urls.append(book_url)
-
-        with open(BOOKS_FILENAME, "a", newline="", encoding="UTF-8-sig") as csvfile:
-            writer = csv.writer(csvfile, delimiter=";", quotechar='"')
-
-            progress_bar_books = tqdm(listed_books, desc = f"Now scraping elements from page {current_page}",position = 1, leave = False)
-            with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
-                future_to_url = {executor.submit(deep_book_scraper, b_url): b_url for b_url in books_urls}
-
-                for future in as_completed(future_to_url):
-                    current_url = future_to_url[future]
-                    try:
-                        book_object = future.result()
-                        if book_object:
-                            writer.writerow([*book_object.to_list(), current_url])
-                            all_books.append(book_object)
-                    except Exception as e:
-                        tqdm.write(f"Error scraping {current_url}: {e}")
-
-                    progress_bar_books.update(1)
-
-
+    progress_bar_pages.close()
+    progress_bar_books.close()
     print(f"Found {len(all_books)} books:\n")
 
     return all_books
 
 if __name__== '__main__':
     detailed_book_list = book_scraper()
+    input("Press enter to continue...")
     for element in detailed_book_list:
         print(element)
 
     print("\n\n")
     input("Press enter to exit...")
+
+
+#        progress_bar_books = tqdm(listed_books, desc = f"Now scraping elements from page {current_page}",position = 1, leave = False)
--- new_directory/DeepBookScraper.py
+++ new_directory/DeepBookScraper.py
@@ -60,29 +60,38 @@ class Book:
                 self.upc]
 
 
-def deep_book_scraper(url) -> Book:
-    # TODO gestire eventuali URL mancanti
-    response = requests.get(url)
+def deep_book_scraper(url) -> Book | None:
+    if not url: return None
+
     try:
-        soup = BeautifulSoup(response.text, "lxml")
-    except:
-        soup = BeautifulSoup(response.text, "html.parser")
-    article = soup.find("article", class_="product_page")
+        response = requests.get(url, timeout=5)
+        response.raise_for_status()
 
-    # hardcoded, non ne vado fiero
-    title = article.find("div", class_="col-sm-6 product_main").h1.get_text()
-    price = article.find("p", class_="price_color").get_text()
-    rating = article.find("p", class_="star-rating")["class"][1]
-    description = article.find_all("p")[3].get_text()
+        try:
+            soup = BeautifulSoup(response.text, "lxml")
+        except:
+            soup = BeautifulSoup(response.text, "html.parser")
 
-    table = article.find("table", class_="table")
-    rows = table.find_all("tr")
-    upc = rows[0].td.text
-    availability = rows[5].td.get_text()
+        article = soup.find("article", class_="product_page")
+        if not article: return None
 
-    return Book(title=title,
-                price=price,
-                rating=rating,
-                availability=availability,
-                upc=upc,
-                description=description)
+        # hardcoded, non ne vado fiero
+        title = article.find("div", class_="col-sm-6 product_main").h1.get_text()
+        price = article.find("p", class_="price_color").get_text()
+        rating = article.find("p", class_="star-rating")["class"][1]
+        description = article.find_all("p")[3].get_text()
+
+        table = article.find("table", class_="table")
+        rows = table.find_all("tr")
+        upc = rows[0].td.text
+        availability = rows[5].td.get_text()
+
+        return Book(title=title,
+                    price=price,
+                    rating=rating,
+                    availability=availability,
+                    upc=upc,
+                    description=description)
+
+    except Exception as e:
+        return None
