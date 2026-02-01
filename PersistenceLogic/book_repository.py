00a4384711e44f1913f19719fe5581f5effc0229
Samu <Samuele@DESKTOP-SAM.(none)>
2026-02-01 02:15:27 +0100

    Rimosso il codice duplicato in main.py
    Ora main si occupa solo di gestire le richieste HTTP, le query sono
    diventate metodi della classe BookRepository

--- PersistenceLogic/book_repository.py
+++ PersistenceLogic/book_repository.py
@@ -12,35 +12,6 @@ class BookRepository(DatabaseConnector):
         super().__init__()
         self._create_table()
 
-    @contextmanager
-    def _get_cursor(self):
-        connection = self.connection_pool.getconn()
-        try:
-            with connection.cursor() as cursor:
-                yield cursor
-            connection.commit()
-        except psycopg2.Error as e:
-            connection.rollback()
-            print("Errore DB: ",e)
-            raise
-        finally:
-            connection.connection_pool.putconn(connection)
-
-
-    def insert_book(self, book: Book):
-        # perché l'SQL injection è una cosa brutta e non voglio avere in descrizione 'OR 1=1; DROP TABLE books;--
-        query = """
-                INSERT INTO books (title, price, description, rating, availability, upc, url)
-                VALUES (%s, %s, %s, %s, %s, %s, %s)
-                ON CONFLICT (upc) DO UPDATE SET title       = excluded.title,
-                                                price       = EXCLUDED.price,
-                                                description = EXCLUDED.description,
-                                                rating      = EXCLUDED.rating,
-                                                url         = EXCLUDED.url \
-                """
-        with self._get_cursor() as cursor:
-            cursor.execute(query)
-
     def _create_table(self):
         query = """
                 CREATE TABLE IF NOT EXISTS books
@@ -55,3 +26,62 @@ class BookRepository(DatabaseConnector):
                 );"""
         with self._get_cursor() as cursor:
             cursor.execute(query)
+
+    @contextmanager
+    def _get_cursor(self):
+        connection = self.connection_pool.getconn()
+        try:
+            with connection.cursor() as cursor:
+                yield cursor
+            connection.commit()
+        except psycopg2.Error as e:
+            connection.rollback()
+            print("Errore DB: ",e)
+            raise
+        finally:
+            connection.connection_pool.putconn(connection)
+
+    def insert_book(self, book: Book):
+        # perché l'SQL injection è una cosa brutta e non voglio avere in descrizione 'OR 1=1; DROP TABLE books;--
+        query = """
+                INSERT INTO books (title, price, description, rating, availability, upc, url)
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
+                ON CONFLICT (upc) DO UPDATE SET title       = excluded.title,
+                                                price       = EXCLUDED.price,
+                                                description = EXCLUDED.description,
+                                                rating      = EXCLUDED.rating,
+                                                url         = EXCLUDED.url \
+                """
+        with self._get_cursor() as cursor:
+            cursor.execute(query, book.to_list())
+
+    def get_stats(self):
+        query = """
+                SELECT ROUND(AVG(price), 2) FROM public.books;
+                SELECT COUNT(*) FROM public.books
+                """
+        with self._get_cursor() as cursor:
+            cursor.execute(query)
+            avg_price, count = cursor.fetchone()
+            return {"Number of books": count,
+                    "Average price": float(avg_price) if avg_price else 0}
+
+    def get_book(self, upc: str):
+        query = """
+                SELECT title, price, description, rating, availability, upc, url
+                FROM public.books WHERE upc = %s
+                """
+        with self._get_cursor() as cursor:
+            cursor.execute(query, (upc,))
+            book = cursor.fetchone()
+            if book:
+                return {
+                    "title": book[0],
+                    "price": book[1],
+                    "description": book[2],
+                    "rating": book[3],
+                    "availability": book[4],
+                    "upc": book[5],
+                    "url": book[6],
+                }
+            return None
--- main.py
+++ main.py
@@ -18,46 +18,13 @@ db = BookRepository()
 def homepage():
     return {"messaggio": "Welcome to Book API!"}
 
-
 @app.get("/stats")
 def get_stats():
-    connection = db.connection_pool.getconn()
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT ROUND(AVG(price), 2) FROM public.books")
-            avg_price = cursor.fetchone()[0]
-            cursor.execute("SELECT COUNT(*) FROM public.books")
-            count_books = cursor.fetchone()[0]
-        return {
-            "number_of_books": count_books,
-            "avg_price": avg_price,
-        }
-    finally:
-        db.connection_pool.putconn(connection)
-
+    return db.get_stats()
 
 @app.get("/book/{upc}")
 def get_book(upc: str):
-    connection = db.connection_pool.getconn()
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute(
-                "SELECT title, price, description, rating, availability, upc, url FROM public.books WHERE upc = %s",
-                (upc,))
-            book = cursor.fetchone()
-            if book:
-                return {
-                    "title": book[0],
-                    "price": book[1],
-                    "description": book[2],
-                    "rating": book[3],
-                    "availability": book[4],
-                    "upc": book[5],
-                    "url": book[6],
-                }
-            raise HTTPException(status_code=404, detail="Book not found")
-    finally:
-        db.connection_pool.putconn(connection)
+    db.get_book(upc)
 
 
 if __name__ == "__main__":
